34a8b0f9ccfdd6b20c8be53fa8bed4008735a9dc
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-28 22:47:33 -0400

    added classname for styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,18 @@ import {Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import pika from './images/pika.png'
+import useStyles from './styles'
 
 // functional component
 const App = () => {
+    const classes = useStyles()
+    
     return (
 
         <Container maxWidth='lg'>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={pika} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={pika} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
