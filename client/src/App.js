ec2d547740d509bcddb66f0cd91e36f4baa10f81
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-28 22:14:16 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+// functional component
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from './routes/posts.js'
+
+// initialize app
+const app = express();
+
+// set up middleware for /posts routes
+app.use('/posts', postRoutes)
+
+//bodyParser will allow to properly send request
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//
+const CONNECTION_URL =
+  "mongodb+srv://admin:1JlkPHZuuRgiihtP@cluster0.phfrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
