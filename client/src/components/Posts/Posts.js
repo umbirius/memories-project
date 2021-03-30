493870791e396e8ec54bcfcc7e2b2e03ad45bd00
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-29 23:39:47 -0400

    filled out post card

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -23,7 +23,7 @@ const Posts = () => {
       >
         {posts.map((post) => (
           <Grid key={post._id} item xs={12} sm={6}>
-            <Post post={post} />
+            <Post post={post}/>
           </Grid>
         ))}
       </Grid>
