170b9dcfc59d3b2977fb711e64ffbc13a0530f84
Umberto Palazzo <upalazzo93@gmail.com>
2021-03-30 00:25:25 -0400

    added update

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -22,3 +22,16 @@ export const createPost = (post) => async (dispatch) => {
         console.log(error)
     }
 }
+
+// update post
+export const updatePost = (id, post) => async (dispatch) => {
+    try{
+        // create post api request to server
+        const {data} = await api.updatePost(id, post)
+        dispatch({ type: 'UPDATE', payload:data})
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
