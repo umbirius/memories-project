f2d65953534e3e805de8ae2cd15d0840c0a41c44
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-29 22:53:21 -0400

    added base64

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-// import FileBase from 'react-file-base64'
+import FileBase from 'react-file-base64'
 import { useDispatch} from 'react-redux'
 import useStyles from "./styles";
 import {createPost} from '../../actions/posts'
