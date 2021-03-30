20c09f86141593d8049590b19678fc2848e81642
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-29 23:10:43 -0400

    added ui componts pass down prop to post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
 
 import useStyles from "./styles";
@@ -8,15 +8,26 @@ import useStyles from "./styles";
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
-  
-  console.log(posts)
+
+  console.log(posts);
   return (
     // react fragment
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+    !posts.length ? (
+      <CircularProgress />
+    ) : (
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
 
