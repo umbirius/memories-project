22253c60145994a6fa1bb513ab5c963a4215efcf
Umberto Palazzo <upalazzo93@gmail.com>
2021-03-30 23:19:59 -0400

    submit or edit auto clears form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -33,9 +33,13 @@ const Form = ({ currentId, setCurrentId }) => {
     } else {
       dispatch(createPost(postData));
     }
+    clear()
   };
 
-  const clear = () => {};
+  const clear = () => {
+    setCurrentId(null)
+    setPostData({ creator: '', title: '', message: '', tags:'', selectedFile: '' })
+  };
 
   return (
     <Paper className={`${classes.root} ${classes.form}`}>
