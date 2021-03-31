9e432529e3c132c5c6ed9a733048cff0c4835b0c
Umberto Palazzo <upalazzo93@gmail.com>
2021-03-30 23:16:13 -0400

    added ternary to form header

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -45,7 +45,7 @@ const Form = ({ currentId, setCurrentId }) => {
         className={classes.form}
         onSubmit={handleSubmit}
       >
-        <Typography variant="h6">Creating a Memory</Typography>
+        <Typography variant="h6">${currentId ? 'Editing' : 'Creating'}</Typography>
         <TextField
           name="creator"
           variant="outlined"
