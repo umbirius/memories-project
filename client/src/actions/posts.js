49f79dc9fed6e5f7e95f912292c0120a00dce8ec
Umberto Palazzo <upalazzo93@gmail.com>
2021-03-31 22:50:47 -0400

    imported actionTypes to actions

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE} from '../constants/actionTypes'
 import * as api from "../api";
 
 // Action creators
@@ -6,7 +7,7 @@ export const getPosts = () => async (dispatch) => {
     // get response from api
     const { data } = await api.fetchPosts();
 
-    dispatch({ type: "FETCH_ALL", payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -17,7 +18,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     // create post api request to server
     const { data } = await api.createPost(post);
-    dispatch({ type: "CREATE", payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -28,7 +29,7 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     // create post api request to server
     const { data } = await api.updatePost(id, post);
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error); // error gives more info than error.message
   }
@@ -40,7 +41,7 @@ export const deletePost = (id) => async (dispatch) => {
     await api.deletePost(id);
 
     // no need to const because we do not need any return data, only deleting
-    dispatch({ type: "DELETE", payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
@@ -50,7 +51,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error);
   }
