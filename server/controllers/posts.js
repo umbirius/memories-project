6db4d93a88556309d986fa28433604719302c6b9
Umberto Palazzo <upalazzo93@gmail.com>
2021-03-31 21:01:00 -0400

    added deletePost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -40,3 +40,14 @@ export const updatePost = async (req, res) => {
 
   res.json(updatePost);
 };
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("no post with that id");
+
+    await PostMessage.findByIdAndRemove(id)
+
+    res.json({ message: 'Post deleted successfully'})
+};
