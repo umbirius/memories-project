0fec8fdd57ca707e307b4ab6f3a457136d29c54c
Umberto Palazzo <upalazzo93@gmail.com>
2021-03-31 23:01:08 -0400

    variables moved to .env

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv"
 
 import postRoutes from './routes/posts.js'
 
 // initialize app
 const app = express();
+dotenv.config()
 
 //bodyParser will allow to properly send request
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -17,11 +19,11 @@ app.use(cors());
 app.use('/posts', postRoutes)
 
 //
-const CONNECTION_URL =
-  "mongodb+srv://admin:1JlkPHZuuRgiihtP@cluster0.phfrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://admin:1JlkPHZuuRgiihtP@cluster0.phfrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
   )
